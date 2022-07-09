b45ae42fce7d4d7e79877f84892e7a02e7eba2d4
João Carlos Schwab Zanardi <jonh.dev.br@gmail.com>
2022-07-09 15:49:25 -0300

    Spring Boot

---
+++ Spring Boot/SpringBoot Security/dio-spring-security/src/main/java/dio/springsecurity/init/StartApplication.java
@@ -0,0 +1,42 @@
+package dio.springsecurity.init;
+
+import dio.springsecurity.model.User;
+import dio.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if (user == null){
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("admin123");
+            user.getRoles().add("MANAGERS");
+            repository.save(user);
+        }
+
+        user = repository.findByUsername("user");
+        if (user == null){
+            user = new User();
+            user.setName("USER");
+            user.setUsername("jonh");
+            user.setPassword("jonh123");
+            user.getRoles().add("USERS");
+            repository.save(user);
+
+        }
+    }
+
+}
